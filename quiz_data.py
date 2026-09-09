f0b88054f7ae48c8731ca29b82da7aa5d29ece47
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-09-09 23:19:45 +0100

    adding code, will take data from question csv and store them

---
+++ quiz_data.py
@@ -0,0 +1,18 @@
+import csv #import csv module so it can read from questions.csv file
+
+def load_questions(filepath='questions.csv'): #creating function to load questions from csv file
+    questions = [] #empty list to store questions
+
+    with open(filepath,newline='',encoding='utf=8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader: #will loop for each row in questions.csv file
+            questions.append({ #adding to the empty list
+                "questions":row["questions"],
+                "options":[
+                    row["option1"],
+                    row["option2"],
+                    row["option3"],
+                    row["option4"],
+                ],
+                "answer":int(row[{answer}])-1 #index is always 0 so -1
+            })
