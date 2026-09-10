725e55d9d5b1bab35b14bfa0c2d4d4ce9f06d5ca
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-09-10 21:52:59 +0100

    character checking name functions

--- test_smoke_and_all.py
+++ test_smoke_and_all.py
@@ -10,6 +10,13 @@ class TestSmoke(unittest.TestCase):
         questions=load_questions()
         self.assertIsNotNone(questions)
 
+    def test_character_check_happy(self):
+        self.assertTrue(character_check("Natasha"))
+        self.assertTrue(character_check("Natasha Zinyuke"))
+
+    def test_character_check_unhappy(self):
+        self.assertTrue(character_check("Natasha001"))
+        self.assertTrue(character_check("#Natashaisthenumber1best"))
 
 if __name__=="__main__":
     unittest.main(verbosity=2)
