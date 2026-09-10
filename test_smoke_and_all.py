7f23e8529e98b92e5c4927eba100a8fd0896baf9
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-09-10 21:43:01 +0100

    automating testing for questions loading

--- test_smoke_and_all.py
+++ test_smoke_and_all.py
@@ -6,5 +6,9 @@ class TestSmoke(unittest.TestCase):
     def test_load_questions_runs(self):
         self.assertTrue(1)
 
+    def test_load_questions_runs(self):
+        questions=load_questions()
+        self.assertIsNotNone(questions)
+
 if __name__=="__main__":
-    unittest.main(verbosity=1)
+    unittest.main(verbosity=2)
